00761ff00a3c955b75a96d73e4fd7e3043ab0210
Firuz <you@example.com>
2023-11-05 01:58:03 +0800

    supa

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,7 +1,7 @@
 // utils/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://fyzcarugqmcpcfspouzk.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5emNhcnVncW1jcGNmc3BvdXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkxMDY2NDMsImV4cCI6MjAxNDY4MjY0M30.AFVqzMGr9Sxor29w80a_9A7fJy-3MaanWqNpLPRl5lE";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
